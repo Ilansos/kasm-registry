acac113993d9014baf6848a4a7ad3961d7916e8a
Ilansos <155109753+Ilansos@users.noreply.github.com>
2024-08-31 16:20:29 +0300

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,10 +2,10 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    name: 'Ilan registry',
+    description: 'unofficial KASM registry',
+    icon: '/img/logo.jpg',
+    listUrl: 'https://ilansos.github.io/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
